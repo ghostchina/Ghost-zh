79cc8caf8af818fced9e50df771e7cf043e78931
wangsai <wangsai@bootcss.com>
2015-01-14 22:21:15 +0800

    默认开启 插入代码 功能

--- core/client/controllers/settings.js
+++ core/client/controllers/settings.js
@@ -13,8 +13,8 @@ var SettingsController = Ember.Controller.extend({
     showNavigation: Ember.computed('session.user.name', 'config.navigationUI', function () {
         return this.get('session.user.isAuthor') || this.get('session.user.isEditor') || !this.get('config.navigationUI') ? false : true;
     }),
-    showCodeInjection: Ember.computed('session.user.name', 'controllers.feature.codeInjectionUI', function () {
-        return this.get('session.user.isAuthor') || this.get('session.user.isEditor') || !this.get('controllers.feature.codeInjectionUI') ? false : true;
+    showCodeInjection: Ember.computed('session.user.name', function () {
+        return this.get('session.user.isAuthor') || this.get('session.user.isEditor') ? false : true;
     }),
     showLabs: Ember.computed('session.user.name', function () {
         return this.get('session.user.isAuthor')  || this.get('session.user.isEditor') ? false : true;
