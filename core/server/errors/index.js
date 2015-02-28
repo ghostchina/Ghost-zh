37697abc25783ea14e4ec8b2dabd28bc6ebfa055
wangsai <wangsai@bootcss.com>
2015-03-01 04:08:40 +0800

    汉化

--- core/server/errors/index.js
+++ core/server/errors/index.js
@@ -284,7 +284,7 @@ errors = {
     },
 
     error404: function (req, res, next) {
-        var message = res.isAdmin && req.user ? 'No Ghost Found' : 'Page Not Found';
+        var message = res.isAdmin && req.user ? 'No Ghost Found' : '页面未找到';
 
         // do not cache 404 error
         res.set({'Cache-Control': 'no-cache, private, no-store, must-revalidate, max-stale=0, post-check=0, pre-check=0'});
