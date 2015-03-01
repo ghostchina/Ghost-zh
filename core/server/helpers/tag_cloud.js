c7f18b08fff098de899a1af76f390c44452bc2e2
wangsai <wangsai@bootcss.com>
2015-03-01 17:55:09 +0800

    完善 tag_cloud 助手

--- core/server/helpers/tag_cloud.js
+++ core/server/helpers/tag_cloud.js
@@ -2,47 +2,30 @@
 // Usage: `{{tag_cloud limit="5"}}`
 // Defaults to limit="5"
 
-var Promise         = require('bluebird'),
-    _               = require('lodash'),
-    hbs             = require('express-hbs'),
+var _               = require('lodash'),
     template        = require('./template'),
     config          = require('../config'),
     api             = require('../api'),
-    dataProvider    = require('../models'),
     tag_cloud;
 
 tag_cloud = function (options) {
-    var limit = (options && options.hash.limit) || 'all';
+    var tagCloudOptions = (options || {}).hash || {};
 
-    if(limit !== 'all') {
-        limit = parseInt(limit, 10) || 5;
-    }
+    tagCloudOptions = _.pick(tagCloudOptions, ['limit']);
+    tagCloudOptions = {
+        limit: (_.has(tagCloudOptions, 'limit') && !/all/i.test(tagCloudOptions.limit))? parseInt(tagCloudOptions.limit, 10) : 'all',
+        include: ['post_count'].join(','),
+        context: 'internal'
+    };
 
-    var collection = dataProvider.Tags.forge();
+    return api.tags.browse(tagCloudOptions).then(function(tags){
+        var sortedTags = _.sortBy(tags.tags, 'post_count').reverse();
 
-    return collection.query(function(qb) {
-      qb.select()
-        .count('tags.id as post_count')
-        .from('tags')
-        .innerJoin('posts_tags', 'tags.id', '=', 'posts_tags.tag_id')
-        .groupBy('tags.name')
-        .orderBy('post_count', 'desc');
-
-        if(_.isNumber(limit)) {
-            qb.limit(limit);
-        }
-    }).fetch().then(function(collection) {
-        var tags = collection.toJSON();
-
-        tags = _.map(tags, function (tag) {
-            return {
-                href: config.urlFor('tag', {tag: tag}),
-                text: _.escape(tag.name),
-                count: tag.post_count
-            };
+        sortedTags.forEach(function(){
+            this.url = config.urlFor('tag', {tag: this}, false);
         });
 
-        return template.execute('tag_cloud',  {tags:tags});
+        return template.execute('tag_cloud',  {tags:sortedTags});
     });
 };
 
