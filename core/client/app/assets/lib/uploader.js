b854b2a6845c8159443e11a6a3e34797cbf47541
王赛 <wangsai@bootcss.com>
2015-04-21 14:16:44 +0800

    fix typo

--- core/client/app/assets/lib/uploader.js
+++ core/client/app/assets/lib/uploader.js
@@ -6,7 +6,7 @@ var UploadUi,
 
 UploadUi = function ($dropzone, settings) {
     var $url = '<div class="js-url"><input class="url js-upload-url" type="url" placeholder="http://"/></div>',
-        $cancel = '<a class="image-cancel js-cancel" title="Delete"><span class="hidden">Delete</span></a>',
+        $cancel = '<a class="image-cancel js-cancel" title="删除"><span class="hidden">删除</span></a>',
         $progress =  $('<div />', {
             class: 'js-upload-progress progress progress-success active',
             role: 'progressbar',
